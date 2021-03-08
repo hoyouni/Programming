a298a95717fad66e3b431215269357b852a5dde6
hoyouni <sgm00006@gmail.com>
2021-03-08 17:54:28 +0900

    게시물 댓글에 대한 CRUD 작업

--- ex03/src/main/java/org/zerock/mapper/ReplyMapper.java
+++ ex03/src/main/java/org/zerock/mapper/ReplyMapper.java
@@ -1,5 +1,23 @@
 package org.zerock.mapper;
 
-public interface ReplyMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyVO;
+
+public interface ReplyMapper {
+	// Create
+	public int insert(ReplyVO vo);
+	
+	// Read
+	public ReplyVO read(Long bno);
+	
+	// Delete
+	public int delete(Long rno);
+	
+	// Update
+	public int update(ReplyVO reply);
+	
+	public List<ReplyVO> getListWithPaging(@Param("cri") Criteria cri, @Param("bno") Long bno);
 }
